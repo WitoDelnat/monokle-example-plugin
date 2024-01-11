0aa77c1cb3f360da28525ca67adb26f336c6814e
Delnat Wito <wito.delnat@gmail.com>
2024-01-11 13:22:49 +0100

    chore: update

--- src/plugin.ts
+++ src/plugin.ts
@@ -2,11 +2,11 @@ import { definePlugin } from "@monokle/plugin-toolkit";
 import { noEmptyAnnotations } from "./rules/1-example.js";
 
 export default definePlugin({
-  id: "YCP",
-  name: "ycp",
+  id: "ABC",
+  name: "monokle-plugin",
   displayName: "Your custom plugin",
   description: "Welcome to your first plugin!",
   rules: {
-    noEmptyAnnotations
+    noEmptyAnnotations,
   },
 });
--- src/rules/1-example.ts
+++ src/rules/1-example.ts
@@ -6,7 +6,7 @@ import { defineRule } from "@monokle/plugin-toolkit";
 export const noEmptyAnnotations = defineRule({
   id: 1,
   description: "Require annotations as metadata.",
-  help: "Add any annotation to the Kubernetes resource.",
+  help: "Add an annotation to the Kubernetes resource.",
   validate({ resources }, { report }) {
     resources.forEach((resource) => {
       const annotations = Object.entries(resource.metadata?.annotations ?? {});
